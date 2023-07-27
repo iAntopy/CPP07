e70de71ad2654a23b9b1b422d6c29be00d9ccb28
Ian Antony Mongeon Charest <ian.mongeon42@gmail.com>
2023-07-27 08:15:36 -0400

    CPP07 review DONE. This time for real.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 22:48:57 by iamongeo          #+#    #+#             */
-/*   Updated: 2023/07/24 22:56:43 by iamongeo         ###   ########.fr       */
+/*   Updated: 2023/07/30 19:46:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,29 @@
 
 int main( void )
 {
-	int a = 2;
-	int b = 3;
+	int a = 2, b = 3;
+	float c = 2.987f, d = 3.123f;
+	double e = -0.123, f = 0.123;
+	std::string g = "chain111", h = "chain2";
 
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+
+	::swap(g, h);
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
+	std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
 	return 0;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 22:59:22 by iamongeo          #+#    #+#             */
-/*   Updated: 2023/07/30 19:21:03 by marvin           ###   ########.fr       */
+/*   Updated: 2023/07/30 19:51:08 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@
 /// iterated Test elements and multiply each by itself.
 struct Test
 {
-	public:
-		int		x;
-		float	y;
-		double	z;
+	int		x;
+	float	y;
+	double	z;
 
-		Test(int x, float y, double z): x(x), y(y), z(z) {}
-		Test	operator*(Test& other) {return (Test(x * other.x, y * other.y, z * other.z));}
+	Test(int x, float y, double z): x(x), y(y), z(z) {}
+	Test	operator*(Test& other) {return (Test(x * other.x, y * other.y, z * other.z));}
 };
 
 std::ostream&	operator<<(std::ostream& o, const Test& t) {
@@ -35,33 +34,33 @@ std::ostream&	operator<<(std::ostream& o, const Test& t) {
 
 int main()
 {
-    int     int_arr[6] = {6, 5, 4, 3, 2, 1};
-    float   float_arr[9] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f};
-    Test    test_arr[4] = { Test(1, 2.2f, 4.999999999),
-                            Test(0, 0.0f, 0.0),
-                            Test(42, -42.42f, -99999999999.999999999),
-                            Test(-999, 0.000000000000000000123f, 0.01100)};
-    
-    std::cout << "Edge cases: " << std::endl;
-  //  std::cout << "Calling iter with NULL pointer array: " << std::endl;
-    //iter(NULL, 5, print_elem);// DOES NOT COMPILE
-    std::cout << "Calling iter with valid pointer array with length 0: " << std::endl;
-    iter(int_arr, 0, print_elem);
+	int     int_arr[6] = {6, 5, 4, 3, 2, 1};
+	float   float_arr[9] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 6.6f, 7.7f, 8.8f, 9.9f};
+	Test    test_arr[4] = { Test(1, 2.2f, 4.999999999),
+													Test(0, 0.0f, 0.0),
+													Test(42, -42.42f, -99999999999.999999999),
+													Test(-999, 0.000000000000000000123f, 0.01100)};
+	
+	std::cout << "Edge cases: " << std::endl;
+//  std::cout << "Calling iter with NULL pointer array: " << std::endl;
+	//iter(NULL, 5, print_elem);// DOES NOT COMPILE
+	std::cout << "Calling iter with valid pointer array with length 0: " << std::endl;
+	iter(int_arr, 0, print_elem);
 
-    std::cout << std::endl << "Printing int array" << std::endl;
-    iter(int_arr, 6, print_elem);
-    std::cout << std::endl << "Printing float array" << std::endl;
-    iter(float_arr, 9, print_elem);
-    std::cout << std::endl << "Printing Test array" << std::endl;
-    iter(test_arr, 4, print_elem);
-    std::cout << std::endl;
+	std::cout << std::endl << "Printing int array" << std::endl;
+	iter(int_arr, 6, print_elem);
+	std::cout << std::endl << "Printing float array" << std::endl;
+	iter(float_arr, 9, print_elem);
+	std::cout << std::endl << "Printing Test array" << std::endl;
+	iter(test_arr, 4, print_elem);
+	std::cout << std::endl;
 
-    std::cout << std::endl << "Printing squared int array" << std::endl;
-    iter(int_arr, 6, print_square);
-    std::cout << std::endl << "Printing squared float array" << std::endl;
-    iter(float_arr, 9, print_square);
-    std::cout << std::endl << "Printing squared Test array" << std::endl;
-    iter(test_arr, 4, print_square);
-    std::cout << std::endl;
-    return (0);
+	std::cout << std::endl << "Printing squared int array" << std::endl;
+	iter(int_arr, 6, print_square);
+	std::cout << std::endl << "Printing squared float array" << std::endl;
+	iter(float_arr, 9, print_square);
+	std::cout << std::endl << "Printing squared Test array" << std::endl;
+	iter(test_arr, 4, print_square);
+	std::cout << std::endl;
+	return (0);
 }
